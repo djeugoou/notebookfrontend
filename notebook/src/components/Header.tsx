dcb21031d319fbc7ccb43b3c8b66c3edb7cee28f
djeugoou <djeugoustella@gmail.com>
2025-09-30 19:17:30 +0200

    button component from shadcn

--- notebook/src/components/Header.tsx
+++ notebook/src/components/Header.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
 
 const Header=()=>{
-    return(<>
+    return(
+    <>
     <div>
+        <header className="bg-white shadow-lg ">
+            <div className="mx-auto px-4 sm:px-6 lg:px-8 py-4">
+                <div className="flex justify-between items-center ">
+                    <h1>
+                        NOTE
+                    </h1>
+                    <Button variant="outline"> <Plus/>New Note</Button>
+                </div>
+                
+            </div>
+        </header>
+     
 
     </div>
     </>
